3a5acffd2fd3167cde8b5c9ee09b37cd167ec22e
Jakub Zakrzewski <j.zakrzewsky@gmail.com>
2023-08-29 11:01:09 +0200

    rwd

--- src/components/Home/HomeMenu.vue
+++ src/components/Home/HomeMenu.vue
@@ -50,11 +50,31 @@ const store = useStore();
   flex-direction: row;
   justify-content: space-between;
 
+  @include md {
+    flex-direction: column;
+    align-items: center;
+  }
+  @include sm {
+    flex-direction: column;
+    align-items: center;
+  }
+
   &__column {
     width: 32%;
     display: flex;
     flex-direction: column;
     gap: 15px;
+
+    @include md {
+      width: 80%;
+      gap: 10px;
+      margin-bottom: 10px;
+    }
+    @include sm {
+      width: 80%;
+      gap: 10px;
+      margin-bottom: 10px;
+    }
   }
 }
 </style>
